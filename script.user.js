c77f48ff18cf58f11ad7aa1355aa1d667124b083
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2022-02-22 18:01:49 +0100

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -9,6 +9,6 @@
 // ==/UserScript==
 
 setInterval(() => {
-    document.querySelector(`div[tabindex="0"][role="group"]`).style.display = "none";
+    document.querySelector(`#layers > div.css-1dbjc4n.r-aqfbo4.r-1d2f490.r-12vffkv.r-1xcajam.r-zchlnj.r-ipm5af > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-g6jmlv`).style.display = 'none';
     document.getElementsByTagName('html')[0].style.overflow = 'auto';
 }, 1000);
